1e72f23c4a3448463af324497d4712e97c1c9b25
Dududous <drummeduardo@gmail.com>
2024-02-16 18:15:39 -0300

    segundo commit

---
+++ classes.py
@@ -0,0 +1,26 @@
+class classe():
+    def __init__(self,nome,idade,tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+
+    def atacar(self):
+
+        match tipo:
+            case "mago":
+                print("O Mago atacou usando magia")    
+            case "guerreiro":
+                print("O Mago atacou usando espada")
+            case "monge":
+                print("O Mago atacou usando artes marciais")   
+            case "ninja":
+                print("O Ninja atacou usando shuriken")    
+
+nome = input("Digite o nome do herói: ")
+idade = input("Digite a idade do herói: ")
+tipo = input("Digite a classe do herói: ")
+
+heroi = classe(nome,idade,tipo)
+
+heroi.atacar()
+
